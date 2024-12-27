1a34f45cf902e72af4d4efc1b2770bb912b0b211
JeremiasBertoldi <jeremiasbertoldi9@gmail.com>
2024-12-26 22:25:12 -0300

    pom

--- main/page-objects/dashboard.ts
+++ main/page-objects/dashboard.ts
@@ -1,19 +1,33 @@
-import { Page, expect } from '@playwright/test';
+import { Locator, Page, expect } from '@playwright/test';
 
 export class DashboardPage {
     private page: Page;
-    static goToHomepage: any; 
+    private elements: { [key: string]: Locator };
+
   
     constructor(page: Page) {
       this.page = page;
+
+      // Define the elements that we will interact with
+        this.elements = {
+            logo: this.page.locator('img[alt="InMotion Hosting Logo"]'),
+            sidebar: this.page.locator('i.fa-imh-menu-red'),
+            navBarDropdown: this.page.locator('#navbarNavDropdown'),
+            blogPosts: this.page.locator('.latest-blog-posts'),
+            headerManuList: this.page.locator('#navbarNavDropdown .nav1 .nav-item'),
+        }
     }
   
+    async getWebElement(key: string): Promise<Locator> {
+        return this.elements[key];
+    }
+
     async goToHomepage() {
       await this.page.goto('https://www.inmotionhosting.com/');
     }
   
-    async assertTitle() {
-      await expect(this.page).toHaveTitle(/InMotion Hosting | Premier Web Host & Server Provider/);
+    async assertTitle(title: string) {
+      await expect(this.page).toHaveTitle(title);
     }
   
     async assertLogoVisible() {
@@ -35,8 +49,20 @@ export class DashboardPage {
       const navBarDropdown = await this.page.locator('#navbarNavDropdown');
       await expect(navBarDropdown).toBeVisible();
     }
+
+    async returnHeaderList() {
+        return this.elements.headerManuList;
+    }
   
-    async assertBlogPostsVisible() {
+    async assertHeaderDropdownVisible(elmnt: String) {
+        switch (elmnt.trim().toLocaleLowerCase()) {
+            case 'blogposts':
+                await expect(this.elements.blogPosts).toBeVisible();
+                break;
+            default:
+                throw new Error('Element not found');
+                break;
+        }
       const blogPosts = await this.page.locator('.latest-blog-posts');
       await expect(blogPosts).toBeVisible();
     }
--- tests/dashboard.test.ts
+++ tests/dashboard.test.ts
@@ -1,20 +1,20 @@
 import { expect, test } from '@playwright/test';
-import { DashboardPage as dashboard} from '../main/page-objects/dashboard';
+import { DashboardPage } from '../main/page-objects/dashboard';
+import assert from 'assert';
 
-test.beforeEach(async () => {
+let dashboard : DashboardPage;
+
+test.beforeEach(async ({page}) => {
+  dashboard = new DashboardPage(page);
   await dashboard.goToHomepage();
 });
 
-test('it loads', async ({ page }) => {
-  await expect(page).toHaveTitle(/InMotion Hosting | Premier Web Host & Server Provider/);
-  const logo = await page.locator('img[alt="InMotion Hosting Logo"]');
-  await expect(logo).toBeVisible();
+test('it loads', async () => {
+  await dashboard.assertTitle("InMotion Hosting | Premier Web Host & Server Provider");
+  await dashboard.assertLogoVisible();
 });
 
-test('it has a side bar menu', async ({ page }) => {
-  const sidebar = await page.locator('i.fa-imh-menu-red');
-  await expect(sidebar).toBeVisible();
-  sidebar.click();
+test('it verifies header dropdowns are visible', async ({ page }) => {
   const navBarDropdown = await page.locator('#navbarNavDropdown');
   await expect(navBarDropdown).toBeVisible();
 });
@@ -23,5 +23,3 @@ test('it displays the latest blog posts', async ({ page }) => {
   const blogPosts = await page.locator('.latest-blog-posts');
   await expect(blogPosts).toBeVisible();
 });
-
-
